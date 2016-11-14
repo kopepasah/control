67547b5092a9ea159868a98e20d72a19f9d3360d
Justin Kopepasah <justin@kopepasah.com>
2016-11-14 19:35:59 +0900

    Convert to NPM package and add sync options.

---
+++ gruntfile.js
@@ -0,0 +1,28 @@
+module.exports = function( grunt ) {
+	var path = require( 'path' );
+
+	/**
+	 * Keep Grunt tasks in their own directory (tasks) and include them all at
+	 * once, including registering the tasks.
+	 * See: https://www.npmjs.com/package/load-grunt-config
+	 */
+	require( 'load-grunt-config' )( grunt, {
+		configPath : path.join( process.cwd(), 'tasks' ),
+
+		data : {
+			location : {
+				home    : path.join( __dirname, '..' ),
+				control : path.join( __dirname ),
+				configs : path.join( __dirname, 'configs' ),
+				tools   : path.join( __dirname, 'tools' ),
+			},
+		},
+
+		preMerge : function( config, data ) {},
+	} );
+
+	// Register grunt tasks.
+	grunt.registerTask(
+		'default', []
+	);
+}
---
+++ tasks/sync.js
@@ -0,0 +1,31 @@
+/**
+ * Module: grunt-sync
+ * Documentation: https://www.npmjs.com/package/grunt-sync
+ */
+
+var path = require( 'path' );
+
+module.exports = {
+	/**
+	 * Command: grunt sync:configs
+	 * Action: Copys content from Control's configs to the users home.
+	 */
+	configs : {
+		verbose : true,
+		updateAndDelete : false,
+		compareUsing: 'md5',
+		files : [
+			{
+				cwd : '<%= location.configs %>',
+				dest : '<%= location.home %>',
+				src : [
+					'.bash_profile',
+					'.bashrc',
+					'.gitconfig',
+					'.inputrc',
+					'.vimrc',
+				],
+			}
+		]
+	},
+};
